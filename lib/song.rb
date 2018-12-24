373e129fa539fcbac07c839924816df03d64f6aa
Meisuleen Kased <meisuleen@gmail.com>
2018-12-24 15:01:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,9 +4,10 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
+  extend Memorable::ClassMethods
+
   @@songs = []
 
-extend Memorable::ClassMethods
 
   def initialize
     @@songs << self
